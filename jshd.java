1e6e2a878213515d0e2ab4c3b91daa6cd4c6d62c
Ryzo1226 <vinceylim1226@gmail.com>
2025-09-09 22:19:37 +0800

    .

---
+++ jshd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class jshd {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int x = 2;
+    int y = 3;
+        System.out.print(x+" ");
+        x = x+y;
+        System.out.print(x+" ");
+        x = x+y;
+        x++;
+        System.out.print(x+" ");
+        x = x+y+y;
+        x--;
+        System.out.print(x+" ");
+        x = x+y+y;
+        System.out.print(x+" ");
+        x = x+y+y;
+        x++;
+        System.out.print(x+" ");
+
+    }
+}
