d71eede0e1b3e568b25ee3ffdc6118a6d5013590
Ryzo1226 <vinceylim1226@gmail.com>
2025-11-29 23:09:05 +0800

    .

---
+++ java/Java.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Java {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("");
+        
+    }
+
+}
---
+++ java/jo.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class jo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String fname, lname;
+        double height,weight,BMI,square;
+        String choice;
+
+        do {
+            System.out.println("Enter First name: ");
+            fname = sc.nextLine();
+            System.out.println("Enter Last name: ");
+            lname = sc.nextLine();
+            System.out.println("Enter Height(m): ");
+            height = sc.nextDouble();
+            System.out.println("Enter Weight(kg): ");
+            weight = sc.nextDouble();
+            System.out.println("Hello "+fname+" "+lname);
+            square = height*height;
+            BMI = weight/square;
+            System.out.println("Your BMI is "+BMI);
+            //sc.nextLine();
+            System.out.println("Do you want to continue? (yes/no)");
+            choice = sc.next();
+        } while (choice.equalsIgnoreCase("yes"));
+
+        System.out.println("Goodbye!");
+        sc.close();
+    }
+}
